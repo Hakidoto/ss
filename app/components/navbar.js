66e45022076cb03728932aa53410a84586a9bb8a
Hadid Garcia <hadid.garcia1@gmail.com>
2023-10-06 22:56:38 -0600

    Selector del navbar funcionando
    ok pero no navbar bugueada?
    
    Signed-off-by: Hadid Garcia <hadid.garcia1@gmail.com>

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -17,35 +17,52 @@ import {
   DropdownMenu,
   DropdownItem,
 } from "@nextui-org/react";
+import { usePathname } from "next/navigation";
 
 export default function NavbarTRC() {
+  const pathname = usePathname();
+  const menuItems = [
+    { nombre: "Inicio", link: "/" },
+    { nombre: "Cuestionarios", link: "/cuestionario" },
+    { nombre: "Manuales", link: "/manuales" },
+    { nombre: "Administracion", link: "/administracion" },
+  ];
+
   return (
-    <Navbar isBordered>
+    <Navbar
+      classNames={{
+        item: [
+          "flex",
+          "relative",
+          "h-full",
+          "items-center",
+          "data-[active=true]:after:content-['']",
+          "data-[active=true]:after:absolute",
+          "data-[active=true]:after:bottom-0",
+          "data-[active=true]:after:left-0",
+          "data-[active=true]:after:right-0",
+          "data-[active=true]:after:h-[2px]",
+          "data-[active=true]:after:rounded-[2px]",
+          "data-[active=true]:after:bg-danger",
+        ],
+      }}
+      isBordered
+    >
       <NavbarContent justify="start">
         <NavbarBrand className="mr-4">
           <p className="hidden sm:block font-bold text-inherit">ACME</p>
         </NavbarBrand>
         <NavbarContent className="hidden sm:flex gap-3">
-          <NavbarItem>
-            <Link color="foreground" href="/">
-              Inicio
-            </Link>
-          </NavbarItem>
-          <NavbarItem isActive>
-            <Link href="cuestionario" aria-current="page" color="secondary">
-              Cuestionarios
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="manuales">
-              Manuales
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="administracion">
-              Administracion
-            </Link>
-          </NavbarItem>
+          {menuItems.map((item, index) => {
+            const isActive = pathname === item.link;
+            return (
+              <NavbarItem key={`${item}-${index}`} isActive={isActive}>
+                <Link color={isActive ? "danger" : "foreground"} href={item.link}>
+                  {item.nombre}
+                </Link>
+              </NavbarItem>
+            );
+          })}
         </NavbarContent>
       </NavbarContent>
 
