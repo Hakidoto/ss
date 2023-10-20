c613dcd44adf9a738ed7c2bded299da2dec8cca7
Hadid Garcia <hadid.garcia1@gmail.com>
2023-10-20 17:16:42 -0600

    Cambios en el api y nuevas vistas de edicion de encuestas
    Todo:
    - Realizar ajustes a la tarjeta y a la tabla de sql para procesar las respuestas de otra forma, ya que actualmente la tabla trae solo datos del personal

--- app/cuestionario/[id]/edit/cardpregunta.js
+++ app/cuestionario/[id]/edit/cardpregunta.js
@@ -12,62 +12,91 @@ import {
   SelectItem,
 } from "@nextui-org/react";
 
-export default function CardPregunta() {
+import React, { cache, use } from "react";
+import { useEffect, useState } from "react";
+
+const getQuestions = (id) => {
+  // Ensure you return the promise from fetch
+  return fetch(`/api/cuestionario/respuesta/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      // Check for a successful response (status code 200)
+      if (!res.ok) {
+        throw new Error(`Request failed with status: ${res.status}`);
+      }
+      // Parse the response as JSON
+      return res.json();
+    });
+};
+
+
+export default function CardPregunta({ pregunta }) {
+  const [answerData, setAnswerData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      // Assuming getSurveys returns an array of questions objects
+      const data = await getQuestions(pregunta.survey_id);
+
+      setAnswerData(data);
+    } catch (error) {
+      // Handle error if needed
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []); // Fetch data when the component mounts
+  
   return (
     <>
-      <div class="flex items-center justify-center">
+      <div class="flex items-center justify-center mb-5">
         <Card
           isBlurred
           className="border-none bg-background/60 dark:bg-default-100/50 w-5/6"
         >
-          <CardHeader className=" ml-3">
-              <div className="flex flex-row justify-between">
-                <Input
-                  className="max-w-lg"
-                  label="Pregunta"
-                  placeholder="Ingresa el nombre de la pregunta"
-                />
-                <Select
-                  label="Tipo de pregunta"
-                  placeholder="Selecciona un tipo de pregunta"
-                  className="max-w-xs ml-4"
-                >
-                  {selectorEstatus.map((estatus) => (
-                    <SelectItem key={estatus.value} value={estatus.value}>
-                      {estatus.label}
-                    </SelectItem>
-                  ))}
-                </Select>
-              </div>
+          <CardHeader className="ml-3 flex items-center justify-between">
+            <div className="w-2/3">
+              <Input
+                className="w-full"
+                label="Pregunta"
+                placeholder="Ingresa el nombre de la pregunta"
+                value={pregunta.question_text}
+              />
+            </div>
+            <div className="w-1/3 ml-4 mr-3">
+              <Select
+                label="Tipo de pregunta"
+                placeholder="Selecciona un tipo de pregunta"
+              >
+                {selectorEstatus.map((estatus) => (
+                  <SelectItem key={estatus.value} value={estatus.value}>
+                    {estatus.label}
+                  </SelectItem>
+                ))}
+              </Select>
+            </div>
           </CardHeader>
+
           <CardBody>
             <Input
               autoFocus
-              label="Email"
-              placeholder="Enter your email"
+              label="Respuesta 1"
+              placeholder="Ingresa respuesta 1"
               variant="bordered"
+              className=" mb-3"
             />
             <Input
-              endContent={
-                <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-              }
-              label="Password"
-              placeholder="Enter your password"
-              type="password"
+              label="Respuesta 2"
+              placeholder="Ingresa respuesta"
               variant="bordered"
+              className=" mb-3"
             />
-            <div className="flex py-2 px-1 justify-between">
-              <Checkbox
-                classNames={{
-                  label: "text-small",
-                }}
-              >
-                Remember me
-              </Checkbox>
-              <Link color="primary" href="#" size="sm">
-                Forgot password?
-              </Link>
-            </div>
           </CardBody>
         </Card>
       </div>
--- app/cuestionario/[id]/edit/page.js
+++ app/cuestionario/[id]/edit/page.js
@@ -5,24 +5,41 @@ import CardPregunta from "./cardpregunta";
 import React, { cache, use } from "react";
 import { useEffect, useState } from "react";
 
-
 const getQuestions = cache(() =>
   fetch("/api/cuestionario/pregunta").then((res) => res.json())
 );
 
-
 export default function Page() {
   const [questionData, setQuestionData] = useState([]);
 
+  const fetchData = async () => {
+    try {
+      // Assuming getSurveys returns an array of questions objects
+      const data = await getQuestions();
+
+      setQuestionData(data);
+    } catch (error) {
+      // Handle error if needed
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []); // Fetch data when the component mounts
 
   return (
-    <Card className="mx-auto my-auto flex-1 w-full">
-      <CardHeader className="flex items-center justify-center">
-        <h2 className="text-md">Edicion de encuesta</h2>
-      </CardHeader>
-      <CardBody>
-        <CardPregunta></CardPregunta>
-      </CardBody>
-    </Card>
+    <>
+      <Card className="mx-auto my-auto flex-1 w-full">
+        <CardHeader className="flex items-center justify-center">
+          <h2 className="text-md">Edicion de encuesta</h2>
+        </CardHeader>
+        <CardBody>
+          {questionData.map((question, index) => (
+            <CardPregunta key={index} pregunta={question} />
+          ))}
+        </CardBody>
+      </Card>
+    </>
   );
 }
