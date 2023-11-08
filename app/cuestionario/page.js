0723ea1aeba571931e7fed53fecb886229b02b9b
AlejandroSegovia73 <113955103+AlejandroSegovia73@users.noreply.github.com>
2023-11-08 16:32:18 -0600

    Agregando vistas del login (#12)
    Co-authored-by: Alejandro <xplosion676@gmail.com>
    Co-authored-by: Hadid Garcia <65918935+Hakidoto@users.noreply.github.com>

--- app/cuestionario/page.js
+++ app/cuestionario/page.js
@@ -7,7 +7,7 @@ export default function Page() {
   return (
     <Card className="mx-auto my-auto flex-1">
       <CardHeader className="flex items-center justify-center">
-        <h2 className="text-md">Cuestionarios disponibles</h2>
+        <h2 className="text-md">Cuestionarios disponibless</h2>
       </CardHeader>
       <CardBody>
         <PanelCuestionario ></PanelCuestionario>
