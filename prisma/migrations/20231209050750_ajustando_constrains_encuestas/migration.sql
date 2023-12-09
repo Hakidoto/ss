c5e7a9bf4636f913db9676751fc65086cefc68fb
Hadid Garcia <hadid.garcia1@gmail.com>
2023-12-08 23:08:05 -0600

    Añadiendo constrains en base de datos

---
+++ prisma/migrations/20231209050750_ajustando_constrains_encuestas/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE `answers` DROP FOREIGN KEY `answers_survey_id_fk`;
+
+-- DropForeignKey
+ALTER TABLE `survey_responses` DROP FOREIGN KEY `survey_responses_ibfk_2`;
+
+-- AddForeignKey
+ALTER TABLE `answers` ADD CONSTRAINT `answers_survey_id_fk` FOREIGN KEY (`survey_id`) REFERENCES `surveys`(`survey_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `survey_responses` ADD CONSTRAINT `survey_responses_ibfk_2` FOREIGN KEY (`survey_id`) REFERENCES `surveys`(`survey_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
