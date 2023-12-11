a7b77a622c6cf2152285fa11402dc97182ef5f86
Alejandro <xplosion676@gmail.com>
2023-12-11 16:04:41 -0600

    Modificacion visual del panel de usuarios
    -Se elimino la opcion para editar usuarios

--- app/admi/panelusuarios/page.js
+++ app/admi/panelusuarios/page.js
@@ -1,43 +1,18 @@
 "use client";
 
-import { Button, Card, CardBody, CardHeader } from "@nextui-org/react";
+import { Button, Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
 import { PanelUsuario } from "./panelusuarios";
 import { useRouter } from "next/navigation";
 
 export default function Admi() {
   const router = useRouter();
-  const handleEditUser = () => {
-    // Lógica para editar usuario
-    router.push("/admi/panelusuarios/resetpassword");
-    console.log("Editar usuario");
-  };
-
-  const handleCreateUser = () => {
-    // Lógica para dar de alta usuario
-    router.push("/admi/panelusuarios/signup");
-    console.log("Dar de alta usuario");
-  };
   return (
     <Card className="mx-auto my-auto flex-1 min-h-[80vh]">
-      <CardHeader className="flex items-center justify-center">
+      <CardHeader className="flex items-center justify-center mt-2">
         <h2>Administrar Usuarios</h2>
       </CardHeader>
+      <Divider />
       <CardBody>
-        <div className="p-4 text-center">
-          <Button auto size="large" onClick={handleEditUser} color="primary">
-            Editar Usuario
-          </Button>
-          <Button
-            auto
-            size="large"
-            color="primary"
-            onClick={handleCreateUser}
-            className="ml-4"
-            
-          >
-            Dar de Alta
-          </Button>
-        </div>
         <PanelUsuario></PanelUsuario>
       </CardBody>
     </Card>
