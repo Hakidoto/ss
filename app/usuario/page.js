c72d90a4345e7b87ecdb2ebb3d3f8583780ddfc4
Hadid Garcia <hadid.garcia1@gmail.com>
2023-12-12 16:21:21 -0600

    prevenir que cargue la sesion despues del fetch

--- app/usuario/page.js
+++ app/usuario/page.js
@@ -1,13 +1,21 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
-import { Table, Card, CardHeader, CardBody, Tabs, Tab, Button } from '@nextui-org/react';
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  Card,
+  CardHeader,
+  CardBody,
+  Tabs,
+  Tab,
+  Button,
+} from "@nextui-org/react";
 import style from "./components/style/CardU.module.css";
-import PersonalData from './components/PersonalData';
-import ContactData from './components/ContactData';
-import StatusData from './components/StatusData';
-import WorkExperience from './components/WorkExperience';
-import { useSession } from 'next-auth/react';
+import PersonalData from "./components/PersonalData";
+import ContactData from "./components/ContactData";
+import StatusData from "./components/StatusData";
+import WorkExperience from "./components/WorkExperience";
+import { useSession } from "next-auth/react";
 
 export default function Page() {
   const { data: session, status } = useSession();
@@ -18,42 +26,49 @@ export default function Page() {
   const [loading, setLoading] = useState(true);
   const [isEditable, setIsEditable] = useState(false);
 
-
   async function fetchData() {
     setLoading(true);
-    try {
-      const UsuarioId = session.user.id; // Reemplaza con el ID del usuario que deseas obtener
-      const response = await fetch(`/api/usuario/${UsuarioId}`);
-  
-      if (response.ok) {
-        const user = await response.json();
-        setUser(user);
-        setUserRfc(user.RFC)
-        setLoading(false);
-        // Luego de obtener los datos del usuario, puedes llamar a fetchUserExpData
-        await fetchUserExpData(user);
-      } else {
-        console.error('Error al obtener datos de la API');
+    if (session) {
+      try {
+        const UsuarioId = session.user.id; // Reemplaza con el ID del usuario que deseas obtener
+        const response = await fetch(`/api/usuario/${UsuarioId}`);
+
+        if (response.ok) {
+          const user = await response.json();
+          setUser(user);
+          setUserRfc(user.RFC);
+          setLoading(false);
+          // Luego de obtener los datos del usuario, puedes llamar a fetchUserExpData
+          await fetchUserExpData(user);
+        } else {
+          console.error("Error al obtener datos de la API");
+        }
+      } catch (error) {
+        console.error("Error al conectarse a la API", error);
       }
-    } catch (error) {
-      console.error('Error al conectarse a la API', error);
     }
   }
 
   async function fetchUserExpData(user) {
-    try {
-      const rfc = user.RFC;
-      const response = await fetch(`/api/usuario/experienciaLaboral/experiencia/${encodeURIComponent(user.RFC)}`);
-      if (response.ok) {
-        const data = await response.json();
-        setUserExp(data);
-        setUserRfc(rfc)
-        setLoading(false);
-      } else {
-        console.error('Error al obtener datos de la API');
+    if (session) {
+      try {
+        const rfc = user.RFC;
+        const response = await fetch(
+          `/api/usuario/experienciaLaboral/experiencia/${encodeURIComponent(
+            user.RFC
+          )}`
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setUserExp(data);
+          setUserRfc(rfc);
+          setLoading(false);
+        } else {
+          console.error("Error al obtener datos de la API");
+        }
+      } catch (error) {
+        console.error("Error al conectarse a la API", error);
       }
-    } catch (error) {
-      console.error('Error al conectarse a la API', error);
     }
   }
 
@@ -76,34 +91,62 @@ export default function Page() {
     },
   ];
 
-  
-
   useEffect(() => {
-    fetchData()
+    fetchData();
   }, []);
 
   useEffect(() => {
-    console.log(user)
+    console.log(user);
   }, [userRfc]);
 
-
-  
-  
   const renderTabContent = (item) => {
     switch (item.id) {
       case "personalData":
-        return <PersonalData user = {user} isEditable = {isEditable} userId = {session.user.id} fetchData= {fetchData} setIsEditable={setIsEditable}/>;
+        return (
+          <PersonalData
+            user={user}
+            isEditable={isEditable}
+            userId={session.user.id}
+            fetchData={fetchData}
+            setIsEditable={setIsEditable}
+          />
+        );
       case "contactData":
-        return <ContactData user = {user} isEditable = {isEditable} userId = {session.user.id} fetchData= {fetchData} setIsEditable={setIsEditable}/>;
+        return (
+          <ContactData
+            user={user}
+            isEditable={isEditable}
+            userId={session.user.id}
+            fetchData={fetchData}
+            setIsEditable={setIsEditable}
+          />
+        );
       case "employmentStatus":
-        return <StatusData user = {user} isEditable = {isEditable} userId = {session.user.id} fetchData= {fetchData} setIsEditable={setIsEditable}/>;
+        return (
+          <StatusData
+            user={user}
+            isEditable={isEditable}
+            userId={session.user.id}
+            fetchData={fetchData}
+            setIsEditable={setIsEditable}
+          />
+        );
       case "workExperience":
-        return <WorkExperience userExp={userExp} isEditable = {isEditable} userId = {session.user.id} fetchData= {fetchData} setIsEditable={setIsEditable} loading = {loading} userRfc = {userRfc}/>;
+        return (
+          <WorkExperience
+            userExp={userExp}
+            isEditable={isEditable}
+            userId={session.user.id}
+            fetchData={fetchData}
+            setIsEditable={setIsEditable}
+            loading={loading}
+            userRfc={userRfc}
+          />
+        );
       default:
         return null;
     }
   };
-
 
   return (
     <Card className={`mx-auto my-auto ${style.main_card}`}>
@@ -112,20 +155,31 @@ export default function Page() {
       </CardHeader>
       <CardBody>
         <div className="flex w-full flex-col">
-          <Tabs aria-label="Dynamic tabs" items={tabs} radius='full' color='secondary'>
+          <Tabs
+            aria-label="Dynamic tabs"
+            items={tabs}
+            radius="full"
+            color="secondary"
+          >
             {(item) => (
-              <Tab key={item.id} title={item.label} >
-                <Card >
+              <Tab key={item.id} title={item.label}>
+                <Card>
                   <CardBody className={style.bodyCard}>
-                    {renderTabContent(item)}
+                    {session? renderTabContent(item) : "no data"}
                   </CardBody>
                 </Card>
               </Tab>
             )}
           </Tabs>
         </div>
-        <Button className={isEditable? `w-1/2  ${style.hov}` : `w-1/2`} color='secondary' variant={isEditable?'solid' : 'flat'} size='md' onClick={()=> setIsEditable(!isEditable)}>
-          {isEditable? "Cancelar" : "Editar informacion personal"}
+        <Button
+          className={isEditable ? `w-1/2  ${style.hov}` : `w-1/2`}
+          color="secondary"
+          variant={isEditable ? "solid" : "flat"}
+          size="md"
+          onClick={() => setIsEditable(!isEditable)}
+        >
+          {isEditable ? "Cancelar" : "Editar informacion personal"}
         </Button>
       </CardBody>
     </Card>
