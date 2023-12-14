4ba2001636a772eed5d6f2a5fc4f9241831594a5
Alejandro <xplosion676@gmail.com>
2023-12-14 10:35:58 -0600

    Optimización de formulario de registro de usuarios. -Se simplificó y reestructuró la lógica del formulario, menos campos

--- app/admi/panelusuarios/signup/page.js
+++ app/admi/panelusuarios/signup/page.js
@@ -17,7 +17,7 @@ import { useToast } from "@/components/ui/use-toast";
 
 export default function SignUp() {
   const router = useRouter();
-  const {toast} = useToast();
+  const { toast } = useToast();
   const [formData, setFormData] = useState({
     RFC: "",
     nombre: "",
@@ -95,12 +95,11 @@ export default function SignUp() {
 
   const fields = [
     { name: "RFC", label: "RFC", type: "text", description: "" },
-    { name: "nombre", label: "Nombre", type: "text", description: "" },
     {
       name: "username",
       label: "Nombre de usuario",
       type: "text",
-      description: "",
+      description: "No se puede repetir el nombre de usuario*",
     },
     {
       name: "password",
@@ -108,45 +107,49 @@ export default function SignUp() {
       type: "password",
       description: "",
     },
-    { name: "edad", label: "Edad", type: "text", description: "" },
-    { name: "direccion", label: "Dirección", type: "text", description: "" },
-    { name: "celular", label: "Celular", type: "text", description: "" },
-    { name: "telefono", label: "Teléfono", type: "text", description: "" },
-    {
-      name: "correo",
-      label: "Correo electrónico",
-      type: "email",
-      description: "",
-    },
-    { name: "redSocial", label: "Red social", type: "text", description: "" },
-    {
-      name: "tipoEmpleado",
-      label: "Tipo de empleado",
-      type: "text",
-      description: "",
-    },
-    { name: "contrato", label: "Contrato", type: "text", description: "" },
-    { name: "horario", label: "Horario", type: "text", description: "" },
-    { name: "estado", label: "Estado", type: "text", description: "" },
-    { name: "antiguedad", label: "Antigüedad", type: "text", description: "" },
+    // { name: "nombre", label: "Nombre", type: "text", description: "" },
+    // { name: "edad", label: "Edad", type: "text", description: "" },
+    // { name: "direccion", label: "Dirección", type: "text", description: "" },
+    // { name: "celular", label: "Celular", type: "text", description: "" },
+    // { name: "telefono", label: "Teléfono", type: "text", description: "" },
+    // {
+    //   name: "correo",
+    //   label: "Correo electrónico",
+    //   type: "email",
+    //   description: "",
+    // },
+    // { name: "redSocial", label: "Red social", type: "text", description: "" },
+    // {
+    //   name: "tipoEmpleado",
+    //   label: "Tipo de empleado",
+    //   type: "text",
+    //   description: "",
+    // },
+    // { name: "contrato", label: "Contrato", type: "text", description: "" },
+    // { name: "horario", label: "Horario", type: "text", description: "" },
+    // { name: "estado", label: "Estado", type: "text", description: "" },
+    // { name: "antiguedad", label: "Antigüedad", type: "text", description: "" },
   ];
 
   return (
     <div className="flex items-center justify-center">
-      <Card className="w-[100%]">
+      <Card className="w-[35%]">
         <CardHeader className="flex items-center justify-center mt-2">
           <h2>Registro de nuevos usuarios</h2>
         </CardHeader>
         <Divider />
 
         <CardBody className="overflow-hidden">
-          <form onSubmit={onSubmit} className="grid grid-cols-2 gap-2">
+          <form onSubmit={onSubmit} className="grid gap-2">
+          <div className="grid grid-cols-1 gap-5">
             {fields.map((field, index) => (
               <div key={index}>
                 <Input
                   isRequired
                   description={field.description}
                   autoComplete="off"
+                  labelPlacement={"outside"}
+                  variant="underlined"
                   name={field.name}
                   label={field.label}
                   placeholder={`Ingresa tu ${field.label.toLowerCase()}`}
@@ -156,19 +159,22 @@ export default function SignUp() {
                 />
               </div>
             ))}
-            <div className="flex gap-2 justify-center mt-5 ">
-              <div>
-                <Link href="/admi/panelusuarios/">
-                  <Button size="lg" radius="lg" color="danger">
-                    Volver
-                  </Button>
-                </Link>
-              </div>
-
+          </div>
+            <div className="flex gap-2 justify-center mt-5">
               <div>
                 <Button type="submit" size="lg" radius="lg" color="primary">
                   Confirmar
                 </Button>
+              </div>
+            </div>
+            <div className="flex justify-center mt-2 ">
+              <div>
+                <Link
+                  href="/admi/panelusuarios/"
+                  style={{ textDecoration: "underline", color: "gray" }}
+                >
+                  Volver
+                </Link>
               </div>
             </div>
           </form>
